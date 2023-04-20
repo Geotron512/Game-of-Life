04a146f78ba84367cfb94de6ef703dfff1b44148
Geotron512 <yanshengm2019@gmail.com>
2023-04-20 12:12:02 -0700

    Added stuff

--- src/Frame.java
+++ src/Frame.java
@@ -1,7 +1,38 @@
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
 public class Frame extends JFrame{
+    private JToolBar toolbar = new JToolBar(JToolBar.HORIZONTAL);
+    private Grid grid = new Grid(64, 64, this);
+    private JButton pp = new JButton("Run");
+    private JButton reset = new JButton("Reset");
+    private BorderLayout layout = new BorderLayout();
+    ActionListener action = (e) -> {
+        JButton b = (JButton) e.getSource();
+        if(b == pp){
+            if(!grid.running){
+                grid.running = true;
+                pp.setText("Pause");
+            }
+            else{
+                grid.running = false;
+                pp.setText("Run");
+            }
+        }
+    };
     public Frame(){
-
+        super("Conway's Game of Life");
+        setSize(640, 680);
+        try{UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}catch(Exception exc){System.out.println(exc.getMessage()+"\n"+exc.getCause());}
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(layout);
+        add(grid, "Center");
+        toolbar.add(pp);
+        pp.addActionListener(action);
+        toolbar.add(reset);
+        reset.addActionListener(action);
+        add(toolbar, "South");
+        setVisible(true);
     }
 }
--- src/Grid.java
+++ src/Grid.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 
 public class Grid extends JPanel{
+    public boolean running = false;
     public Grid(int w, int h, Frame f){
 
     }
